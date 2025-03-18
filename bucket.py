df59e7343b03d8808dc1acca4941985e44d5b7aa
anatalchyshyn <149003092+anatalchyshyn@users.noreply.github.com>
2025-03-18 09:26:21 +0200

    Update bucket.py

--- bucket.py
+++ bucket.py
@@ -1,6 +1,9 @@
 import os
 import boto3
-from botocore.exceptions import NoCredentialError
+from botocore.exceptions import NoCredentialError, PartialCredentialsError
+from dotenv import load_dotenv
+
+load_dotenv()
 
 OVH_BUCKET = ""
 OVH_REGION = ""
@@ -37,3 +40,44 @@ def create_bucket(s3_client, bucket_name: str):
     except:
         print("Cannot create a bucket")
         return False
+
+def get_file_from_bucket(s3_client,
+                         bucket_name: str,
+                         object_key: str,
+                         download_path: str):
+    """Download a file from an OVH Object Storage bucket using Boto3."""
+    try:
+        s3_client.download_file(bucket_name, object_key, download_path)
+        print(f"File downloaded successfully: {download_path}")
+        return True
+
+    except NoCredentialsError:
+        print("Error: Credentials not provided or invalid.")
+
+    except PartialCredentialsError:
+        print("Error: Incomplete credentials provided.")
+
+    except Exception as e:
+        print(f"Error downloading file: {e}")
+
+    return False
+
+
+def upload_file_to_bucket(file_path, bucket_name, object_key):
+    """
+    Upload a file to an OVH Object Storage bucket using Boto3.
+    """
+    try:
+
+        s3_client.upload_file(file_path, bucket_name, object_key)
+        print(f"File uploaded successfully: {file_path}")
+        return True
+
+    except NoCredentialsError:
+        print("Error: Credentials not provided or invalid.")
+    except PartialCredentialsError:
+        print("Error: Incomplete credentials provided.")
+    except Exception as e:
+        print(f"Error uploading file: {e}")
+
+    return False
