1d39fca73d1c9d6ada5bfee76c9b9b3755713f57
anatalchyshyn <149003092+anatalchyshyn@users.noreply.github.com>
2025-03-18 09:24:58 +0200

    Update db_creation.py

--- db_creation.py
+++ db_creation.py
@@ -1,46 +1,54 @@
-from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
-from langchain_chroma import Chroma
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
 from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_chroma import Chroma
+import chromadb
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import getpass
 import os
 
-def folder_loader(filepath: str):
+pdf_folder = "../mylife/mylife"
+CHROMA_DB_DIR = "contracts_db"
 
-    loader = PyPDFDirectoryLoader(filepath)
-    docs = loader.load()
-    return docs
+embeddings = GPT4AllEmbeddings()
+chroma_client = chromadb.PersistentClient(CHROMA_DB_DIR)
+collection = chroma_client.get_or_create_collection(name="contracts",)
+
+vector_store_from_client = Chroma(
+    client=chroma_client,
+    collection_name="contracts",
+    embedding_function=embeddings,
+)
+
 
 def single_loader(pdf_folder: str, chunk_size: int, chunk_overlap: int):
-    # split pdf files into chunks and create embeddings
+    """Split pdf files into chunks and create embeddings"""
+
     text_splitter = RecursiveCharacterTextSplitter(
             chunk_size = chunk_size,
             chunk_overlap = chunk_overlap,
             )
 
-    documents = []
-
     for sub_folder in os.listdir(pdf_folder):
+
         for pdf_file in os.listdir(os.path.join(pdf_folder, sub_folder)):
 
             if pdf_file.endswith(".pdf") or pdf_file.endswith(".PDF"):
-
+                print(pdf_file)
                 loader = PyPDFLoader(os.path.join(pdf_folder, sub_folder, pdf_file))
                 docs = loader.load_and_split(text_splitter)
-                documents.extend(docs)
-
-        if len(documents) > 5:
-            return documents
-    return documents
+                metadata = [{"contract_number": sub_folder, "pdf_file": pdf_file} for _ in docs]
+                ids = [sub_folder+"_"+str(i) for i in range(len(docs))]
+                texts = [chunk.page_content for chunk in docs]
+                try:
+                    Chroma.from_texts(texts,
+                                      embeddings,
+                                      ids = ids,
+                                      metadatas=metadata,
+                                      collection_name="contracts",
+                                      persist_directory=CHROMA_DB_DIR)
+                except:
+                    print("Cannot read pdf file")
 
 if __name__ == "__main__":
-
-    documents = single_loader("files/mylife", 300, 50)
-    print("Documents readed")
-    print("Example:")
-    print(documents[0])
-    embeddings = GPT4AllEmbeddings()
-    vector_store = Chroma(collection_name="rag-model",
-                      persist_directory="./chroma_langchain_db",
-                      embedding_function=embeddings)
-    vector_store.add_documents(documents)
-    print("Successfully created vector DB")
+    single_loader(pdf_folder, 500, 100)
